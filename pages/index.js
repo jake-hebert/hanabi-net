1784ec8563c94f32b3d6aaa048255bb3e6727690
Jake H <jake.hebert@callawaycloudconsulting.com>
2020-02-11 09:51:16 -0600

    text prototype

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+export default function Index() {
+  return (
+    <div>
+      <div>
+        <p>Welcome to Hanabi-Net!</p>
+      </div>
+      <br />
+      <div>
+        <button> Start a game </button>
+      </div>
+      <br />
+      <div>
+        <p>
+          Game 12345 - Waiting for players
+          <br />
+          2/4 players
+          <br />
+          <button>Join Game</button>
+        </p>
+      </div>
+      <div>
+        <p>
+          Game 09876 - Waiting for players
+          <br />
+          3/4 players
+          <br />
+          <button>Join Game</button>
+        </p>
+      </div>
+      <div>
+        <p>
+          Game 23232 - playing
+          <br />
+          3/3 players
+        </p>
+      </div>
+    </div>
+  );
+}
